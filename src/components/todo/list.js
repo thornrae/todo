a8ffd6ba04bebdb42b731e726e5459e588bed8ba
Taylor Thornton <thornrae@gmail.com>
2021-06-03 19:44:19 -0700

    react bootstap

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,22 +1,37 @@
 import React from 'react';
 
+import ListGroup from 'react-bootstrap/ListGroup'
+
 function TodoList(props) {
 
     return (
-      <ul>
-        {props.list.map(item => (
-          <li
-            className={`complete-${item.complete.toString()}`}
-            key={item._id}
-          >
-            <span onClick={() => props.handleComplete(item._id)}>
-              {item.text}
-            </span>
-          </li>
-        ))}
-      </ul>
-    );
-  
+
+      <ListGroup variant="flush">
+        {props.list.map(item =>
+        <ListGroup.Item className={`complete-${item.complete.toString()}`} key={item._id} onClick={() => props.handleComplete(item._id)}>{item.text}</ListGroup.Item> 
+          )}
+      </ListGroup>
+
+    )
 }
+
+      
+
+
+    //   <ul>
+    //     {props.list.map(item => (
+    //       <li
+    //         className={`complete-${item.complete.toString()}`}
+    //         key={item._id}
+    //       >
+    //         <span onClick={() => props.handleComplete(item._id)}>
+    //           {item.text}
+    //         </span>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // );
+  
+
 
 export default TodoList;
