2f823176feb9500fdd8b09ad21b75640f06f58c7
Taylor Thornton <thornrae@gmail.com>
2021-06-10 21:48:33 -0700

    added toast bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import SiteContext from './context/context.js';
 
 import ToDo from './components/todo/todo.js';
--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {useState, useContext} from 'react';
 import {SiteContext} from '../../context/context.js';
 
-import ListGroup from 'react-bootstrap/ListGroup'
+// import ListGroup from 'react-bootstrap/ListGroup'
+import Toast from 'react-bootstrap/Toast';
 
 function TodoList(props) {
 
@@ -19,13 +20,32 @@ function TodoList(props) {
     }
 
     return (
-      <ListGroup variant="flush">
-        {props.list.map(item =>
-        <ListGroup.Item className={`complete-${item.complete.toString()}`} key={item._id} onClick={() => props.handleComplete(item._id)}>{item.text} 
-        <span onClick={() => hideComplete(item._id)}>Mark as complete</span>
-        </ListGroup.Item> 
-          )}
-      </ListGroup>
+      <>
+      {props.list.map(item => 
+      <Toast>
+        <Toast.Header>
+      <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+      <strong className="mr-auto">{item.assignee}</strong>
+      <small>{item.complete}</small>
+    </Toast.Header>
+    <Toast.Body className={`complete-item${item.complete.toString()}`} key={item._id} onClick={() => props.handleComplete(item._id) }>{item.text}</Toast.Body>
+    
+  </Toast>
+  )}
+</>
+
+
+
+
+
+
+      // <ListGroup variant="flush">
+      //   {props.list.map(item =>
+      //   <ListGroup.Item className={`complete-${item.complete.toString()}`} key={item._id} onClick={() => props.handleComplete(item._id)}>{item.text} 
+      //   <span onClick={() => hideComplete(item._id)}>Mark as complete</span>
+      //   </ListGroup.Item> 
+      //     )}
+      // </ListGroup>
 
       // <ListGroup variant="flush">
       //   {props.list.map(item =>
